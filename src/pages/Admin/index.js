bbac5f482645e4931b6a939c44a62ed2a38fd1c2
arthur <arthur.massanes@epitech.eu>
2019-07-05 13:00:41 +0200

    moved App.js and removed unnecesary pages

--- src/pages/Admin/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react';
-
-const AdminPageTitle = 'Admin Page';
-const AdminPage = () => (
-  <h1 style={{ backgroundColor: 'grey' }}><b>{AdminPageTitle}</b></h1>
-);
-
-export default AdminPage;
--- src/pages/PasswordForget/index.js
+++
@@ -1,6 +0,0 @@
-// @flow
-// import React from 'react';
-
-const PasswordForgetPage = () => null;
-
-export default PasswordForgetPage;
