8ad2adf19717fd95a73222a35a0f260f16b4d7e6
arthur <arthur.massanes@epitech.eu>
2019-07-05 15:54:51 +0200

    added firebase conf

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBG86Is5EqWxtzvR8HvRxOzzMDV-gJZ5E4',
+  appId: '1:721391041267:web:f27d33a9040c7cc8',
+  authDomain: 'my-web-chat-99ff1.firebaseapp.com',
+  databaseURL: 'https://my-web-chat-99ff1.firebaseio.com',
+  messagingSenderId: '721391041267',
+  projectId: 'my-web-chat-99ff1',
+  storageBucket: 'my-web-chat-99ff1.appspot.com',
+};
